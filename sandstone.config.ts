70b26f69c6d3c36d825bb8ae4fd3ad42b912576f
Mizab <mizabmansari@gmail.com>
2024-01-22 15:58:14 +0530

    Added Mechanism for copying dependencies files

--- sandstone.config.ts
+++ sandstone.config.ts
@@ -1,4 +1,5 @@
 import type { SandstoneConfig } from "sandstone";
+import { execSync } from "child_process";
 
 export default {
   name: "TNTs & Creepers War v1",
@@ -10,5 +11,38 @@ export default {
   saveOptions: { world: "Gravity TNT" },
   onConflict: {
     default: "warn",
+  },
+  scripts: {
+    // afterAll: () => {
+    //   // @ts-ignore
+    //   let worldName = this.default.saveOptions.world;
+    //   if (worldName) {
+    //     let srcDirectoryDatapacks = `dependencies\\datapacks\\*`;
+    //     let destDirectoryDatapacks = `%AppData%\\.minecraft\\saves\\${worldName}\\datapacks\\`;
+    //     let srcDirectoryResources = `dependencies\\resources\\*`;
+    //     let destDirectoryResources = `%AppData%\\.minecraft\\resourcepacks`;
+    //     try {
+    //       execSync(
+    //         `xcopy "${srcDirectoryDatapacks}" "${destDirectoryDatapacks}" /s /e /y`,
+    //         { stdio: "inherit" }
+    //       );
+    //       execSync(
+    //         `xcopy "${srcDirectoryResources}" "${destDirectoryResources}" /s /e /y`,
+    //         { stdio: "inherit" }
+    //       );
+    //       console.log(`Copied: Datapacks to ${destDirectoryDatapacks}`);
+    //       console.log(`Copied: Datapacks to ${destDirectoryResources}`);
+    //     } catch (error) {
+    //       console.error(
+    //         `Error copying ${srcDirectoryDatapacks}: ${error.message}`
+    //       );
+    //       console.error(
+    //         `Error copying ${srcDirectoryResources}: ${error.message}`
+    //       );
+    //     }
+    //   } else {
+    //     console.error("saveOption is set to path and not world");
+    //   }
+    // },
   },
 } as SandstoneConfig;
