b497838c8a5159fb2dafc14ebf39edb3ab1a06e4
Mizab <mizabmansari@gmail.com>
2024-01-22 18:46:08 +0530

    .

--- sandstone.config.ts
+++ sandstone.config.ts
@@ -1,5 +1,5 @@
 import type { SandstoneConfig } from "sandstone";
-import { execSync } from "child_process";
+import { addDependencies } from "./AddDependencies";
 
 export default {
   name: "TNTs & Creepers War v1",
@@ -14,45 +14,11 @@ export default {
   onConflict: {
     default: "warn",
   },
-  // scripts: {
-  //   afterAll: () => {
-  //     // @ts-ignore
-  //     let worldName = this.default.saveOptions.world;
-  //     if (worldName) {
-  //       // Variables for robocopy & command in in the batch file
-  //       // let srcDirectoryDatapacks = `dependencies\\datapacks`;
-  //       // let destDirectoryDatapacks = `%AppData%\\.minecraft\\saves\\${worldName}\\datapacks`;
-
-  //       // let srcDirectoryResources = `dependencies\\resources`;
-  //       // let destDirectoryResources = `%AppData%\\.minecraft\\resourcepacks`;
-
-  //       let srcDirectoryDatapacks = `dependencies\\datapacks\\*`;
-  //       let destDirectoryDatapacks = `%AppData%\\.minecraft\\saves\\${worldName}\\datapacks\\`;
-
-  //       let srcDirectoryResources = `dependencies\\resources\\*`;
-  //       let destDirectoryResources = `%AppData%\\.minecraft\\resourcepacks`;
-  //       try {
-  //         execSync(
-  //           `xcopy "${srcDirectoryDatapacks}" "${destDirectoryDatapacks}" /s /e /y /v`,
-  //           { stdio: "inherit" }
-  //         );
-  //         execSync(
-  //           `xcopy "${srcDirectoryResources}" "${destDirectoryResources}" /s /e /y /v`,
-  //           { stdio: "inherit" }
-  //         );
-  //         console.log(`Copied: Datapacks to ${destDirectoryDatapacks}`);
-  //         console.log(`Copied: Datapacks to ${destDirectoryResources}`);
-  //       } catch (error) {
-  //         console.error(
-  //           `Error copying ${srcDirectoryDatapacks}: ${error.message}`
-  //         );
-  //         console.error(
-  //           `Error copying ${srcDirectoryResources}: ${error.message}`
-  //         );
-  //       }
-  //     } else {
-  //       console.error("saveOption is set to path and not world");
-  //     }
-  //   },
-  // },
+  scripts: {
+    afterAll: () => {
+      // @ts-ignore
+      let worldName = this.default.saveOptions.world;
+      addDependencies(worldName);
+    },
+  },
 } as SandstoneConfig;
