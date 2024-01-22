a436a7c6e9ebb737aceb9a446b03fdc674bd6ecd
Mizab <mizabmansari@gmail.com>
2024-01-22 17:50:32 +0530

    Reverted to testing world

--- sandstone.config.ts
+++ sandstone.config.ts
@@ -8,49 +8,50 @@ export default {
   namespace: "tnts_and_creepers_war",
   packUid: "-1o4aio1",
   // saveOptions: { path: "./.sandstone/output/datapack" },
-  saveOptions: { world: "Gravity TNT" },
+  saveOptions: { world: "Testing 4" },
+  // saveOptions: { world: "Gravity TNT" },
   onConflict: {
     default: "warn",
   },
-  scripts: {
-    afterAll: () => {
-      // @ts-ignore
-      let worldName = this.default.saveOptions.world;
-      if (worldName) {
-        // Variables for robocopy & command in in the batch file
-        // let srcDirectoryDatapacks = `dependencies\\datapacks`;
-        // let destDirectoryDatapacks = `%AppData%\\.minecraft\\saves\\${worldName}\\datapacks`;
+  // scripts: {
+  //   afterAll: () => {
+  //     // @ts-ignore
+  //     let worldName = this.default.saveOptions.world;
+  //     if (worldName) {
+  //       // Variables for robocopy & command in in the batch file
+  //       // let srcDirectoryDatapacks = `dependencies\\datapacks`;
+  //       // let destDirectoryDatapacks = `%AppData%\\.minecraft\\saves\\${worldName}\\datapacks`;
 
-        // let srcDirectoryResources = `dependencies\\resources`;
-        // let destDirectoryResources = `%AppData%\\.minecraft\\resourcepacks`;
+  //       // let srcDirectoryResources = `dependencies\\resources`;
+  //       // let destDirectoryResources = `%AppData%\\.minecraft\\resourcepacks`;
 
-        let srcDirectoryDatapacks = `dependencies\\datapacks\\*`;
-        let destDirectoryDatapacks = `%AppData%\\.minecraft\\saves\\${worldName}\\datapacks\\`;
+  //       let srcDirectoryDatapacks = `dependencies\\datapacks\\*`;
+  //       let destDirectoryDatapacks = `%AppData%\\.minecraft\\saves\\${worldName}\\datapacks\\`;
 
-        let srcDirectoryResources = `dependencies\\resources\\*`;
-        let destDirectoryResources = `%AppData%\\.minecraft\\resourcepacks`;
-        try {
-          execSync(
-            `xcopy "${srcDirectoryDatapacks}" "${destDirectoryDatapacks}" /s /e /y /v`,
-            { stdio: "inherit" }
-          );
-          execSync(
-            `xcopy "${srcDirectoryResources}" "${destDirectoryResources}" /s /e /y /v`,
-            { stdio: "inherit" }
-          );
-          console.log(`Copied: Datapacks to ${destDirectoryDatapacks}`);
-          console.log(`Copied: Datapacks to ${destDirectoryResources}`);
-        } catch (error) {
-          console.error(
-            `Error copying ${srcDirectoryDatapacks}: ${error.message}`
-          );
-          console.error(
-            `Error copying ${srcDirectoryResources}: ${error.message}`
-          );
-        }
-      } else {
-        console.error("saveOption is set to path and not world");
-      }
-    },
-  },
+  //       let srcDirectoryResources = `dependencies\\resources\\*`;
+  //       let destDirectoryResources = `%AppData%\\.minecraft\\resourcepacks`;
+  //       try {
+  //         execSync(
+  //           `xcopy "${srcDirectoryDatapacks}" "${destDirectoryDatapacks}" /s /e /y /v`,
+  //           { stdio: "inherit" }
+  //         );
+  //         execSync(
+  //           `xcopy "${srcDirectoryResources}" "${destDirectoryResources}" /s /e /y /v`,
+  //           { stdio: "inherit" }
+  //         );
+  //         console.log(`Copied: Datapacks to ${destDirectoryDatapacks}`);
+  //         console.log(`Copied: Datapacks to ${destDirectoryResources}`);
+  //       } catch (error) {
+  //         console.error(
+  //           `Error copying ${srcDirectoryDatapacks}: ${error.message}`
+  //         );
+  //         console.error(
+  //           `Error copying ${srcDirectoryResources}: ${error.message}`
+  //         );
+  //       }
+  //     } else {
+  //       console.error("saveOption is set to path and not world");
+  //     }
+  //   },
+  // },
 } as SandstoneConfig;
