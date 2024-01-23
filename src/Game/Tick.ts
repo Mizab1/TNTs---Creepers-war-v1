88276d19969d3fcdcbcd41fe09d081244a3e84a0
Mizab <mizabmansari@gmail.com>
2024-01-23 16:33:08 +0530

    Added night vision

--- src/Game/Tick.ts
+++ src/Game/Tick.ts
@@ -1,7 +1,16 @@
-import { MCFunction, Objective, tellraw } from "sandstone";
+import { MCFunction, Objective, effect, tellraw } from "sandstone";
 
 const GamePrivate = Objective.create("game_pvt", "dummy");
 export const isStarted = GamePrivate("is_started");
+
+const Tick = MCFunction(
+  "game/tick",
+  () => {
+    // Give night vision to players
+    effect.give("@a", "minecraft:night_vision", 10, 1, true);
+  },
+  { runEachTick: true }
+);
 
 // Function to start and end the game
 const startGame = MCFunction("game/start_game", () => {
