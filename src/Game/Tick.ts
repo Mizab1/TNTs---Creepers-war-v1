f9a2ea425c44aec2cc1849fa5649a0da697f9e5e
Mizab <mizabmansari@gmail.com>
2024-01-23 22:38:53 +0530

    Added Bossbar support

--- src/Game/Tick.ts
+++ src/Game/Tick.ts
@@ -1,7 +1,16 @@
-import { MCFunction, Objective, effect, tellraw } from "sandstone";
+import {
+  MCFunction,
+  Objective,
+  bossbar,
+  effect,
+  execute,
+  tellraw,
+} from "sandstone";
+import { countingTimer, settingTimer } from "./Timer/Tick";
 
 const GamePrivate = Objective.create("game_pvt", "dummy");
 export const isStarted = GamePrivate("is_started");
+export const bossbarTimerName: string = "bossbar_timer";
 
 const Tick = MCFunction(
   "game/tick",
@@ -15,10 +24,30 @@ const Tick = MCFunction(
 // Function to start and end the game
 const startGame = MCFunction("game/start_game", () => {
   isStarted.set(1);
+
+  // Transfer the settingTimer to counterTimer
+  countingTimer.set(settingTimer);
+
+  // Display the bossbar and set the max
+  bossbar.add(bossbarTimerName, { text: "Timer" });
+  // @ts-ignore
+  bossbar.set(bossbarTimerName).visible(true);
+  bossbar.set(bossbarTimerName).players("@a");
+  execute.store.result
+    .bossbar(bossbarTimerName, "max")
+    .run.scoreboard.players.get(
+      countingTimer.target,
+      countingTimer.objective.name
+    );
+
   tellraw("@a", { text: "Game started", color: "green" });
 });
 
 const endGame = MCFunction("game/end_game", () => {
   isStarted.set(0);
+
+  // Remove the bossbar
+  bossbar.remove(bossbarTimerName);
+
   tellraw("@a", { text: "Game ended", color: "red" });
 });
