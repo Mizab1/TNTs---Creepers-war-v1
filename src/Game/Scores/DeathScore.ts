2d212e83ee64225b3746b38f0bff22594a420255
Mizab <mizabmansari@gmail.com>
2024-01-24 22:41:22 +0530

    Added essentials

--- src/Game/Scores/DeathScore.ts
+++ src/Game/Scores/DeathScore.ts
@@ -1,22 +1,48 @@
-import { MCFunction, Objective } from "sandstone";
+import { MCFunction, Objective, Selector, _, execute, scoreboard } from "sandstone";
+import { joinedTeam } from "../Teams/Tick";
 
 // ! Will automatically increment as the player dies
-const deathScoreObj = Objective.create("death_score_obj", "deathCount", {
+export const deathScoreObj = Objective.create("death_score_obj", "deathCount", {
   text: "Death Score",
   color: "red",
 });
-const deathScore = deathScoreObj("@s");
+const deathPrivateObj = Objective.create("death_pvt", "dummy");
+
+export const deathScore = deathScoreObj("@s");
+// const deathScoreOrange = deathScoreObj(Selector("@a", { tag: "orange" }));
+// const deathScoreBlue = deathScoreObj(Selector("@a", { tag: "blue" }));
+export const minDeathScore = deathPrivateObj("min_death");
 
 // ! Must be used by executing AS the player
-const incrementDeathScore = MCFunction(
-  "game/scores/increment_death_score",
-  () => {
-    deathScore.add(1);
-  }
-);
-const decrementDeathScore = MCFunction(
-  "game/scores/decrement_death_score",
-  () => {
-    deathScore.remove(1);
-  }
-);
+const incrementDeathScore = MCFunction("game/scores/increment_death_score", () => {
+  deathScore.add(1);
+});
+const decrementDeathScore = MCFunction("game/scores/decrement_death_score", () => {
+  deathScore.remove(1);
+});
+
+export const resetDeathScore = MCFunction("game/scores/reset_death", () => {
+  execute.as("@a").run(() => {
+    deathScore.reset();
+  });
+});
+
+export const initDeathScore = MCFunction("game/scores/init_death_score", () => {
+  execute.as(joinedTeam).run(() => {
+    deathScore.set(0);
+  });
+});
+
+export const setDisplayDeathScore = MCFunction("game/scores/set_display", () => {
+  scoreboard.objectives.setDisplay("sidebar", deathScoreObj.name);
+});
+
+export const clearDisplayDeathScore = MCFunction("game/scores/clear_display", () => {
+  scoreboard.objectives.setDisplay("sidebar");
+});
+
+export const findMinDeathScore = MCFunction("game/scores/find_min_death_score", () => {
+  minDeathScore.set(99999);
+
+  scoreboard.players.operation(minDeathScore.target, minDeathScore.objective.name, "<", joinedTeam, deathScore.objective.name);
+});
