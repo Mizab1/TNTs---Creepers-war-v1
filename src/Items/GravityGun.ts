5e21c903eb126ca0dc27d26429a603d28162f967
Mizab <mizabmansari@gmail.com>
2024-01-24 21:22:43 +0530

    Added Different Gravity Guns

--- src/Items/GravityGun.ts
+++ src/Items/GravityGun.ts
@@ -1,19 +1,37 @@
-import { MCFunction, give } from "sandstone";
+import { MCFunction, NBT, give } from "sandstone";
 import { self } from "../Tick";
 import { i } from "../Utils/Functions";
 
 /**
  * A standalone function to give a gravity gun to the current executing player
  */
-const giveGun = MCFunction("items/gravity_gun/give", () => {
+export const giveGun = MCFunction("items/gravity_gun/give", () => {
+  // Give TNT gravity gun
   give(
     self,
     i("minecraft:warped_fungus_on_a_stick", {
       CustomModelData: 111101,
       gravity_guns: "gravity_gun",
       gravity_power: 1,
+      tnt_gravity_gun: NBT.byte(1),
       display: { Name: '{"text":"TNT Gravity Gun","italic":false}' },
     }),
     1
   );
+
+  // Give mob gravity gun
+  give(
+    self,
+    i("minecraft:warped_fungus_on_a_stick", {
+      CustomModelData: 111101,
+      gravity_guns: "gravity_gun",
+      gravity_power: 1,
+      mob_gravity_gun: NBT.byte(1),
+      display: { Name: '{"text":"Mob Gravity Gun","italic":false}' },
+    }),
+    1
+  );
 });
+
+// ! Changed the implementation of gravity gun: The gravity looks for a specific tag to execute.
+// ! To revert that remove the tag here and in the item trigger function of gravity gun
