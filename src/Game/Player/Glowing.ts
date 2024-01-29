0d13a14ceca31f5c232f210708741f401b44d29e
Mizab <mizabmansari@gmail.com>
2024-01-29 17:40:27 +0530

    Made it a ticking function and changed the tellraw to title

--- src/Game/Player/Glowing.ts
+++ src/Game/Player/Glowing.ts
@@ -1,14 +1,20 @@
-import { MCFunction, _, effect, tellraw } from "sandstone";
+import { MCFunction, _, effect, tellraw, title } from "sandstone";
 import { joinedTeam } from "../Teams/Tick";
 import { countingTimer } from "../Timer/Tick";
 
 const giveGlow = MCFunction("game/player/give_glow", () => {
   effect.give(joinedTeam, "minecraft:glowing", 5, 0, true);
-  tellraw("@a", { text: "Players are glowing!", color: "red" });
+  title("@a").actionbar({ text: "Players are glowing!", color: "red" });
 });
 
-const glowCounter = MCFunction("game/player/glow_counter", () => {
-  _.if(countingTimer.moduloBy(30).equalTo(0), () => {
-    giveGlow();
-  });
-});
+const glowCounter = MCFunction(
+  "game/player/glow_counter",
+  () => {
+    _.if(countingTimer.moduloBy(30).equalTo(0), () => {
+      giveGlow();
+    });
+  },
+  {
+    runEachTick: true,
+  }
+);
