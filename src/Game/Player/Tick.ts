a1466e78a1bac057017d39190769d2bba3274271
Mizab <mizabmansari@gmail.com>
2024-01-29 21:56:29 +0530

    Changed armor giving function

--- src/Game/Player/Tick.ts
+++ src/Game/Player/Tick.ts
@@ -1,9 +1,21 @@
 import { MCFunction, item } from "sandstone";
 import { self } from "../../Tick";
+import { i } from "../../Utils/Functions";
 
 export const addArmor = MCFunction("game/player/add_armor", () => {
-  item.replace.entity(self, "armor.head").with("minecraft:iron_helmet", 1);
-  item.replace.entity(self, "armor.chest").with("minecraft:iron_chestplate", 1);
-  item.replace.entity(self, "armor.legs").with("minecraft:iron_leggings", 1);
-  item.replace.entity(self, "armor.feet").with("minecraft:iron_boots", 1);
+  // @ts-ignore
+  item.replace.entity(self, "armor.head").with(i("minecraft:iron_helmet", { display: { Name: '{"text":"Special Armor"}' } }), 1);
+
+  item.replace
+    .entity(self, "armor.chest")
+    // @ts-ignore
+    .with(i("minecraft:iron_chestplate", { display: { Name: '{"text":"Special Armor"}' } }), 1);
+
+  item.replace
+    .entity(self, "armor.legs")
+    // @ts-ignore
+    .with(i("minecraft:iron_leggings", { display: { Name: '{"text":"Special Armor"}' } }), 1);
+
+  // @ts-ignore
+  item.replace.entity(self, "armor.feet").with(i("minecraft:iron_boots", { display: { Name: '{"text":"Special Armor"}' } }), 1);
 });
